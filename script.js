010881b17336faa7af1590ffc181de1005210ddf
castielbs <castielbatistadasilva@gmail.com>
2025-07-07 17:27:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+  const paragrafo = document.getElementById("mensagem");
+  paragrafo.innerText = "O caos é necessário para que a criação de todas as coisas que você vem pedindo tenham espaço para se desenvolver. ";
+  paragrafo.style.display = "block";
+}
